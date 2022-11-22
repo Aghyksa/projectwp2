04e0d726b67320379b3b91f437884b34993754ab
Lyon <liony244@gmail.com>
2022-11-22 19:11:09 +0700

    Fix Database.

--- latihan_ci.sql
+++ latihan_ci.sql
@@ -1,14 +1,13 @@
-`-- phpMyAdmin SQL Dump
--- version 4.8.0.1
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 05, 2020 at 03:14 AM
--- Server version: 10.1.32-MariaDB
--- PHP Version: 5.6.36
+-- Generation Time: Nov 22, 2022 at 01:10 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.0.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -39,8 +38,7 @@ CREATE TABLE `data_login` (
 --
 
 INSERT INTO `data_login` (`id`, `username`, `password`) VALUES
-(1, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997'),
-(2, 'yoga', '6b3bebb6d418f3726781486e710abee36f1b371b');
+(1, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997');
 
 -- --------------------------------------------------------
 
@@ -58,7 +56,9 @@ CREATE TABLE `data_login_user` (
 --
 
 INSERT INTO `data_login_user` (`int_npm`, `password`) VALUES
-(17111156, '12dea96fec20593566ab75692c9949596833adc9');
+(17111156, '12dea96fec20593566ab75692c9949596833adc9'),
+(17111111, 'd033e22ae348aeb5660fc2140aec35850c4da997'),
+(19210687, 'd033e22ae348aeb5660fc2140aec35850c4da997');
 
 -- --------------------------------------------------------
 
@@ -81,15 +81,10 @@ CREATE TABLE `data_mahasiswa` (
 --
 
 INSERT INTO `data_mahasiswa` (`id`, `int_npm`, `str_nama`, `str_prodi`, `str_kelas`, `int_semester`, `gambar`) VALUES
-(1, 17111156, 'Yoga Afdilla Jamaluddin', 'Teknik Informatika', 'TIF RP 17 CID C', 5, 'Logo-avengers-wallpaper-HD-pictures-download2.jpg'),
-(22, 17111111, 'Andri', 'Teknik Informatika', 'TIF RP 18 CID A', 6, 'Logo-avengers-wallpaper-HD-pictures-download3.jpg'),
-(23, 17111112, 'Reynaldi', 'Teknik Informatika', 'TIF RP 16 B', 4, 'Logo-avengers-wallpaper-HD-pictures-download4.jpg'),
-(24, 17111113, 'Anggi', 'Teknik Industri', 'TI RP 17 A', 3, 'Logo-avengers-wallpaper-HD-pictures-download1.jpg'),
 (25, 17111114, 'Naufal', 'Desain Komunikasi Visual', 'DKV RM 17 A', 5, 'Logo-avengers-wallpaper-HD-pictures-download5.jpg'),
 (26, 17111115, 'Dendi ', 'Teknik Industri', 'TI WK 15 B', 3, 'Logo-avengers-wallpaper-HD-pictures-download6.jpg'),
 (27, 17111116, 'Nazira', 'Teknik Informatika', 'TIF RM 17 B', 5, 'Logo-avengers-wallpaper-HD-pictures-download7.jpg'),
-(28, 17111117, 'Denar', 'Teknik Informatika', 'TIF WK 18 C', 6, 'Logo-avengers-wallpaper-HD-pictures-download8.jpg'),
-(30, 17111118, 'Azhar', 'Teknik Informatika', 'TIF RP 17 CID C', 5, 'Logo-avengers-wallpaper-HD-pictures-download10.jpg');
+(31, 19210687, 'Lion Younes AYP', 'Desain Komunikasi Visual', '19.3A.03', 3, 'BgFyqM7CAAAtfI6.jpg');
 
 --
 -- Indexes for dumped tables
@@ -115,13 +110,13 @@ ALTER TABLE `data_mahasiswa`
 -- AUTO_INCREMENT for table `data_login`
 --
 ALTER TABLE `data_login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `data_mahasiswa`
 --
 ALTER TABLE `data_mahasiswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
